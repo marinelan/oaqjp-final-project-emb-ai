2101286c71c713b1e8726e13a0a5197a3a53fdf5
marinelan <41306540+marinelan@users.noreply.github.com>
2026-04-16 18:40:39 +0300

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,23 +1,22 @@
-import requests
-import json
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
 
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": text_to_analyze } }
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion_detector(self):
+        result1 = emotion_detector('I am glad this happened.')
+        self.assertEqual(result1['dominant_emotion'],'joy')
 
-    response = requests.post(url, json=input_json, headers=headers)
+        result2 = emotion_detector('I am really mad about this.')
+        self.assertEqual(result2['dominant_emotion'],'anger')
 
-    if response.status_code != 200:
-        raise Exception(f"Request failed with status code {response.status_code}")
+        result3 = emotion_detector('I feel disgusted just hearing about this.')
+        self.assertEqual(result3['dominant_emotion'],'disgust')
+        
+        result4 = emotion_detector('I am so sad about this')
+        self.assertEqual(result4['dominant_emotion'],'sadness')
+        
+        result5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result5['dominant_emotion'],'fear')
 
-    data = json.loads(response.text)
-    emotions = data['emotionPredictions'][0]['emotion']
-
-    dominant_emotion = max(emotions, key=emotions.get)
-    emotions['dominant_emotion'] = dominant_emotion
-
-    items = [f"'{k}': {v}" for k, v in emotions.items() if k != "dominant_emotion"]
-    formatted_items = ", ".join(items)
-
-    return f"For the given statement, the system response is {formatted_items}. The dominant emotion is \033[1m{emotions['dominant_emotion']}."
+if __name__ == "__main__":
+    unittest.main(argv=[''], verbosity=2, exit=False)
